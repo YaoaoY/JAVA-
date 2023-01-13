d29c1cb2fc70b2476e5e0a7459b7d6047134f0ac
Yaoyao2024 <3051337060@qq.com>
2023-01-13 13:15:16 +0800

    1_12

---
+++ Test.java
@@ -0,0 +1,11 @@
+//这是瑶瑶子的一个java程序
+class Test{
+	public static void main(String[] args){
+		System.out.println("Test");
+	}
+}
+class Test2{
+	public static void main(String[] args){
+		System.out.println("Test2");
+	}
+}
---
+++ test2023_1_11/src/TestDemo.java
@@ -0,0 +1,9 @@
+public class TestDemo {
+    public static void main(String[] args) {
+        String str="yaoyao";
+        System.out.println("yaoyao"+10+2);//
+        System.out.println(10+2+"yaoyao");//计算机先计算10+2，得出12，再和字符串拼接
+        byte a=2;
+        byte b=3;
+    }
+}
