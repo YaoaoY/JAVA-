e5d0bef3de8f190a1f99c75001432007a9272beb
Yaoyao2024 <3051337060@qq.com>
2023-01-28 10:31:07 +0800

    1_27

---
+++ test1_27/src/com/yayao/Demo.java
@@ -0,0 +1,14 @@
+package com.yayao;
+
+import java.util.Scanner;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: `
+ * Date: 2023-01-27
+ * Time: 22:14
+ */
+public class Demo {
+    Scanner scanner = new Scanner(System.in);
+}
