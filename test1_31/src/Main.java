218ac97cb8c6a9255e6afc7718175106966b1d99
Yaoyao2024 <3051337060@qq.com>
2023-01-31 23:25:21 +0800

    零钱通小程序01

---
+++ test1_31/src/Main.java
@@ -0,0 +1,37 @@
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: ${USER}
+ * Date: ${YEAR}-${MONTH}-${DAY}
+ * Time: ${TIME}
+ */
+class Person{
+    private String name;
+    private int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
+public class Main {
+    public static void main(String[] args) {
+        Person yaoyao = new Person("Yaoyao", 19);
+    }
+}
